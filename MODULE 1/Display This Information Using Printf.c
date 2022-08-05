8a99c5270fe234d47485000d4a03b8da6e8fec14
JatinFefar <110150109+JatinFefar@users.noreply.github.com>
2022-08-05 09:03:02 +0530

    Add files via upload

---
+++ MODULE 1/Display This Information Using Printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("My Name Is Jatin Fefar");
+	printf("\nMy Age Is 30");
+	printf("\nMy Birth Date Is 3rd Feb 1992");
+	printf("\nMy Address Is Matru Krupa B33 Rajkot");
+	return 0;
+}
